03e404da65d06865388ada4bded9252b95993191
Taranpreet Singh <ramghrialoveysingh123@gmail.com>
2025-07-19 14:50:12 +0530

    add theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
   
 
 const geistSans = Geist({
@@ -27,11 +28,17 @@ export default function RootLayout({
     <html  lang="en" suppressHydrationWarning>
       <body
       
-        className={`${geistSans.variable} ${geistMono.variable}  antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-black text-white  antialiased`}
       >
+        <ThemeProvider
+        attribute={"class"}
+        defaultTheme="black"
+        enableSystem
+        >
         <main>
         {children}
         </main>
+        </ThemeProvider>
       </body>
     </html>
   );
